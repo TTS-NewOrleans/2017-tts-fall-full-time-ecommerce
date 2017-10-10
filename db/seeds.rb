48c4aac0af5480c4bd40a1586caca2728966b3f8
Ryan Hageman <improv42@gmail.com>
2017-10-10 09:43:57 -0500

    Add category seeds to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = ["Computers",
+              "Smart Phones",
+              "Televisions",
+              "Game Consoles",
+              "Video Games",
+              "Appliances",
+              "Other"]
+
+categories.each do |category_name|
+  Category.create(name: category_name)
+end
+
+puts "Your database has been seeded."
