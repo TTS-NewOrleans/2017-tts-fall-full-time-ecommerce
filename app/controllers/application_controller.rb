155c958e02b018f23029d9b33e4536a18a9cab25
Ryan Hageman <improv42@gmail.com>
2017-10-12 09:22:01 -0500

    Add Storefront with views
    * All items
    * Items by category
    * Items by brand
    
    Add filtering dropdowns to navbar
    
    Style views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :categories
+  before_action :categories, :brands
 
   def categories
     @categories = Category.order(:name)
+  end
+
+  def brands
+    @brands = Product.pluck(:brand).sort.uniq
   end
 end
