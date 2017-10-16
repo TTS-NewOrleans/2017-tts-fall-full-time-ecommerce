25b211fec0ea1fc5f385b90f3118d2d076ee5a5b
Ryan Hageman <improv42@gmail.com>
2017-10-16 09:31:48 -0500

    Add user and guest account types and limit access for guests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :categories, :brands
+
+  protected
 
   def categories
     @categories = Category.order(:name)
@@ -9,5 +12,10 @@ class ApplicationController < ActionController::Base
 
   def brands
     @brands = Product.pluck(:brand).sort.uniq
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:role])
   end
 end
