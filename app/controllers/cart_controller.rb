7cfccbde752695abdb84a930d1da67bce8e16285
Ryan Hageman <improv42@gmail.com>
2017-10-17 09:27:03 -0500

    Add separate carts through sessions

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,31 +2,42 @@ class CartController < ApplicationController
   before_action :authenticate_user!, except: [:add_to_cart, :view_order]
 
   def add_to_cart
-    line_item = LineItem.create(product_id: params[:product_id], quantity: params[:quantity])
-    line_item.update(line_item_total: (line_item.quantity * line_item.product.price))
+    @order = current_order
+
+    line_item = @order.line_items.new(product_id: params[:product_id], quantity: params[:quantity])
+    @order.save
+    session[:order_id] = @order.id
+
+    line_item.update(line_item_total: (line_item.product.price * line_item.quantity))
+
     redirect_back(fallback_location: root_path)
   end
 
   def view_order
-    @line_items = LineItem.all
+    @line_items = current_order.line_items
   end
 
   def checkout
-    line_items = LineItem.all
-    @order = Order.create(user_id: current_user.id, subtotal: 0)
+    line_items = current_order.line_items
 
-    line_items.each do |line_item|
-      line_item.product.update(quantity: (line_item.product.quantity - line_item.quantity))
-      @order.order_items[line_item.product_id] = line_item.quantity
-      @order.subtotal += line_item.line_item_total
+    if line_items.length != 0
+      current_order.update(user_id: current_user.id, subtotal: 0)
+      @order = current_order
+
+      line_items.each do |line_item|
+        line_item.product.update(quantity: (line_item.product.quantity - line_item.quantity))
+        @order.order_items[line_item.product_id] = line_item.quantity
+        @order.subtotal += line_item.line_item_total
+      end
+
+      @order.save
+
+      @order.update(sales_tax: (@order.subtotal * 0.08))
+      @order.update(grand_total: (@order.sales_tax + @order.subtotal))
+
+      @order.line_items.destroy_all
+      session[:order_id] = nil
     end
-
-    @order.save
-
-    @order.update(sales_tax: (@order.subtotal * 0.08))
-    @order.update(grand_total: (@order.sales_tax + @order.subtotal))
-
-    line_items.destroy_all
   end
 
   def order_complete
